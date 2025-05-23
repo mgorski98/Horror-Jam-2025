33d04dfa5babf977ade9f09297cba3ec1b91b633
mgorski98 <maciej.gorski98@gmail.com>
2025-05-23 15:25:08 +0200

    added Text mesh pro

--- Assets/Scripts/InteractionDetector.cs
+++ Assets/Scripts/InteractionDetector.cs
@@ -2,15 +2,7 @@ using UnityEngine;
 
 public class InteractionDetector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float InteractionRange;
+    public LayerMask InteractablesMask;
+    public InteractableObject CurrentInteractable;
 }
